3ca64cec5a2deeba45859a344ea31ed9dfe20440
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-08-23 04:06:43 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft"){
+        moveDodgerLeft();
+    }else{
+        moveDodgerRight();
+    }
+    // console.log(event);
+  });
+
+// function moveDodgerLeft(){
+//     const dodger = document.getElementById("dodger");
+//     // dodger.style.backgroundColor ="#000000";
+//     dodger.style.backgroundColor = "#FF69B4";
+//     dodger.style.bottom = "100px";
+//     dodger.style.bottom = "0px";
+//     dodger.style.left = "0px";
+
+// }
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        dodger.style.left = `${left + 1}px`;
+
+    }
+
+  }
+
+moveDodgerLeft();
